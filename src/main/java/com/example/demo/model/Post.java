38393db6166f95923bf8801d5b27290bf0d9ea67
e-mgam-e <romaizvekov007@gmail.com>
2026-02-17 01:18:57 +0500

    bootstrap вёрстка

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,10 +1,14 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
-    public Post(String text) {
+    private Date creationDate;
+    public Post(String text, Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
         this.likes = 0;
     }
 
@@ -13,5 +17,10 @@ public class Post {
     }
     public Integer getLikes() {
         return likes;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+        //return new Date(creationDate.getTime());
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.Post;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Arrays;
 
@@ -12,9 +13,9 @@ public class PostService {
     public List<Post> listAllPosts() {
         // Создаем и возвращаем список из трех постов
         return Arrays.asList(
-                new Post("Первый пост: Привет, мир!"),
-                new Post("Второй пост: Изучаю Spring Boot"),
-                new Post("Третий пост: Сентябрь всё ещё горит")
+                new Post("Первый пост: Привет, мир!", new Date()),
+                new Post("Второй пост: Изучаю Spring Boot", new Date()),
+                new Post("Третий пост: Сентябрь всё ещё горит", new Date())
         );
     }
 }
