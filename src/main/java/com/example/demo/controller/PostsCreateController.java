aa6d3671575ce3d3e030ee98c1c758373b8e2f11
e-mgam-e <romaizvekov007@gmail.com>
2026-02-17 08:40:19 +0500

    create new posts(/new + text)

---
+++ src/main/java/com/example/demo/controller/PostsCreateController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PostsCreateController {
+
+    @Autowired
+    private PostService postsService;
+
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String create() {
+        return "create";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text) {
+        postsService.create(text);
+
+        //обратно на главную
+        return "redirect:/";
+    }
+}
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.Post;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Arrays;
@@ -10,12 +11,18 @@ import java.util.Arrays;
 @Service
 public class PostService {
 
+    private List<Post> posts = new ArrayList<>();
+    //блок инициализации первоначальных запросов
+    {
+        posts.add(new Post("Первый пост: Привет, мир!", new Date()));
+        posts.add(new Post("Второй пост: Изучаю Spring Boot", new Date()));
+        posts.add(new Post("Третий пост: Сентябрь всё ещё горит", new Date()));
+    }
     public List<Post> listAllPosts() {
         // Создаем и возвращаем список из трех постов
-        return Arrays.asList(
-                new Post("Первый пост: Привет, мир!", new Date()),
-                new Post("Второй пост: Изучаю Spring Boot", new Date()),
-                new Post("Третий пост: Сентябрь всё ещё горит", new Date())
-        );
+        return posts;
+    }
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
