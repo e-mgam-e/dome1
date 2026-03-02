3716bae04a54c68b4e5d79e53c03b572d7ff671a
e-mgam-e <romaizvekov007@gmail.com>
2026-03-02 23:24:52 +0500

    fixed a bug with access to a non-existent id

--- src/main/java/com/example/demo/service/LikesService.java
+++ src/main/java/com/example/demo/service/LikesService.java
@@ -3,7 +3,9 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import com.example.demo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class LikesService {
@@ -13,9 +15,10 @@ public class LikesService {
     private PostRepository postRepository;
 
     public int like(Long postId) {
-        // Находим пост по индексу (как просили в задании)
         //Post post = postService.listAllPosts().get(postId.intValue());
-        final Post post = postRepository.findById(postId).get();
+//        final Post post = postRepository.findById(postId).get();
+        final Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Post not found with id: " + postId));
         post.setLikes(post.getLikes() + 1);
         postRepository.save(post);
         return post.getLikes();
